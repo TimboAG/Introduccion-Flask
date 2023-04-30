2ae98f99d15dd68eacc2afcb10502c6f33bae3a7
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-04-30 12:21:21 -0300

    aprendiendo python

--- hola.py
+++ hola.py
@@ -4,10 +4,37 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/hola')
 def hola():
     # return "<h1> Hola mundo </h1>"
     mensaje = "Hola mundo con flask"
     return render_template("index.html", msm=mensaje)
+
+
+class Blog:
+    def __init__(self, titulo, desc):
+        self.titulo = titulo
+        self.desc = desc
+
+
+@app.route('/blogs')
+def blog():
+    b1 = Blog(
+        '¿Qué es Python?',
+        'Descripción de Blog 1')
+
+    b2 = Blog(
+        '¿Qué es Flask?',
+        'Descripción de Blog 2')
+
+    blogs = [b1, b2]
+    return render_template('blog.html', blogs=blogs)
+
+@app.route('/saludar')
+@app.route('/saludar/<nombre>')
+@app.route('/saludar/<nombre>/<edad>')
+def saludar(nombre = None, edad = None):
+    return render_template('hola.html', nombre=nombre, edad = edad)
 
 
 if __name__ == "__main__":
